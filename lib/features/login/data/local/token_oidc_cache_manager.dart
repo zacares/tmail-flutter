4da3b810f7c7625552a487789034b6497b841a15
DatDang <tddang@linagora.com>
2025-01-07 10:53:23 +0700

    TF-3348 Move accessToken from persistent storage to session storage for web

--- lib/features/login/data/local/token_oidc_cache_manager.dart
+++ lib/features/login/data/local/token_oidc_cache_manager.dart
@@ -6,13 +6,13 @@ import 'package:tmail_ui_user/features/login/data/extensions/token_oidc_extensio
 import 'package:tmail_ui_user/features/login/domain/exceptions/authentication_exception.dart';
 
 class TokenOidcCacheManager {
-  final TokenOidcCacheClient _tokenOidcCacheClient;
+  final TokenOidcCacheClient tokenOidcCacheClient;
 
-  TokenOidcCacheManager(this._tokenOidcCacheClient);
+  const TokenOidcCacheManager(this.tokenOidcCacheClient);
 
   Future<TokenOIDC> getTokenOidc(String tokenIdHash) async {
     log('TokenOidcCacheManager::getTokenOidc(): tokenIdHash: $tokenIdHash');
-    final tokenCache = await _tokenOidcCacheClient.getItem(tokenIdHash);
+    final tokenCache = await tokenOidcCacheClient.getItem(tokenIdHash);
     log('TokenOidcCacheManager::getTokenOidc(): tokenCache: $tokenCache');
     if (tokenCache == null) {
       throw NotFoundStoredTokenException();
@@ -23,17 +23,17 @@ class TokenOidcCacheManager {
 
   Future<void> persistOneTokenOidc(TokenOIDC tokenOIDC) async {
     log('TokenOidcCacheManager::persistOneTokenOidc(): $tokenOIDC');
-    await _tokenOidcCacheClient.clearAllData();
+    await tokenOidcCacheClient.clearAllData();
     log('TokenOidcCacheManager::persistOneTokenOidc(): key: ${tokenOIDC.tokenId.uuid}');
     log('TokenOidcCacheManager::persistOneTokenOidc(): key\'s hash: ${tokenOIDC.tokenIdHash}');
     log('TokenOidcCacheManager::persistOneTokenOidc(): token: ${tokenOIDC.token}');
-    await _tokenOidcCacheClient.insertItem(tokenOIDC.tokenIdHash, tokenOIDC.toTokenOidcCache());
+    await tokenOidcCacheClient.insertItem(tokenOIDC.tokenIdHash, tokenOIDC.toTokenOidcCache());
     log('TokenOidcCacheManager::persistOneTokenOidc(): done');
   }
 
   Future<void> deleteTokenOidc() async {
-    await _tokenOidcCacheClient.clearAllData();
+    await tokenOidcCacheClient.clearAllData();
   }
 
-  Future<void> closeTokenOIDCHiveCacheBox() => _tokenOidcCacheClient.closeBox();
+  Future<void> closeTokenOIDCHiveCacheBox() => tokenOidcCacheClient.closeBox();
 }
