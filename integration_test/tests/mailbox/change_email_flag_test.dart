dd05bd60be21e19583c04904b7312373d0dd80f1
DatDang <tddang@linagora.com>
2025-04-09 13:47:37 +0700

    fixup! TF-3569 E2E Mailbox toggle read/star/spam

--- integration_test/tests/mailbox/change_email_flag_test.dart
+++
@@ -1,9 +0,0 @@
-import '../../base/test_base.dart';
-import '../../scenarios/mailbox/change_email_flag_scenario.dart';
-
-void main() {
-  TestBase().runPatrolTest(
-    description: 'Should see emails with expected flag after changed',
-    scenarioBuilder: ($) => ChangeEmailFlagScenario($),
-  );
-}
